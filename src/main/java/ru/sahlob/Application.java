2411bd79f4d08bcd1d664e05d1b5ea348aa69683
Лобачев Александр Константинович <AKLobachev@sberbank.ru>
2021-02-02 23:26:18 +0300

    start commit 3

--- src/main/java/ru/sahlob/Application.java
+++ src/main/java/ru/sahlob/Application.java
@@ -2,9 +2,9 @@ package ru.sahlob;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+//@SpringBootApplication(exclude = { org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
